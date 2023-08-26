da52a88c7303527c469153702e98706f974843af
Farid Sesin <faridsesin@gmail.com>
2023-08-26 07:32:14 -0500

    Delete clase15-16 directory

--- clase15-16/src/db/dbConfig.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-
-const URI = 'mongodb+srv://coderhouse:coderhouse@cluster0.sugvijj.mongodb.net/mongoAvan43400?retryWrites=true&w=majority'
-
-mongoose.connect(URI)
-.then(()=>console.log('Conectado a la base de datos'))
-.catch(error=>console.log(error))
