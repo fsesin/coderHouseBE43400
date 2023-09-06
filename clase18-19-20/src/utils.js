377e3ba059e03622be1b573f340ab4b99aa1d32b
Farid Sesin <faridsesin@gmail.com>
2023-09-05 21:03:54 -0500

    Add files via upload

---
+++ clase18-19-20/src/utils.js
@@ -0,0 +1,13 @@
+import {dirname}from 'path'
+import { fileURLToPath } from 'url'
+import bcrypt from 'bcrypt'
+
+export const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export const hashData = async(data)=>{
+    return bcrypt.hash(data,10)
+}
+
+export const compareData = async(data,hashData)=>{
+    return bcrypt.compare(data,hashData)
+}
