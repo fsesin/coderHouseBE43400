c3964a8fe3640a94dd4616a67b260f3b06ae8933
Farid Sesin <faridsesin@gmail.com>
2023-09-21 18:18:56 -0500

    Delete clase18-19-20-21 directory

--- clase18-19-20-21/src/persistencia/mongoDB/dbConfig.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const URI =
-  'mongodb+srv://coderhouse:coderhouse@cluster0.sugvijj.mongodb.net/mongoSession43400?retryWrites=true&w=majority'
-
-mongoose
-  .connect(URI)
-  .then(() => console.log('Conectado a la base de datos'))
-  .catch((error) => console.log(error))
